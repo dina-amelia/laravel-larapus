fcb8d678a76a7da16920c2872fc246b9cc8e2a01
dina-amelia <dinaamelia4124@gmail.com>
2021-11-08 16:23:05 +0700

    Logout

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -527,6 +527,22 @@
               <p>Informational</p>
             </a>
           </li>
+
+            <a href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <p>Logout</p>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
